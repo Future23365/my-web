8dc29a407dccaadbae27bae11ce6a75e63469a94
future23365 <liusongbai2072@qq.com>
2021-08-05 20:20:39 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  123
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'App',
+  components: {
+    
+  }
+})
+</script>
+
+<style>
+#app {
+  
+}
+</style>
